18a8eaa25b31cedb8973a52df9d212494b266114
VadimKurachynski <kurachynski@gmail.com>
2022-06-22 21:43:08 +0300

    2

--- 1.js
+++
@@ -1,8 +0,0 @@
-<scrypt>
-var symbol = 's';
-var isUpperCase = symbol.toUpperCase()==symbol;
-alert(!!isUpperCase);  // true - верхний регистр, false - нижний
-
-console.log('Привет от JavaScript!');
-
-</scrypt>
