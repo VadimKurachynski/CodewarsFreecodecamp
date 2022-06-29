9512ddf25163b2bcf4ea0ae642f75bb79e1e3787
VadimKurachynski <kurachynski@gmail.com>
2022-06-29 21:34:25 +0300

    3

---
+++ codewars/Rock Paper Scissors2.js
@@ -0,0 +1,19 @@
+
+const rps = (p1, p2) => {
+  if (p1 === p2) return "Draw!";
+  var rules = { rock: "scissors", paper: "rock", scissors: "paper" };
+  if (p2 === rules[p1]) {
+
+    return "Player 1 won!";
+  }
+  else {
+    return "Player 2 won!";
+  }
+};
+
+console.log(rps("scissors", 'paper'));
+
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
