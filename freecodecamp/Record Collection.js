85985ac3c9dae245e5d2bd4c3a1d74f855154536
VadimKurachynski <kurachynski@gmail.com>
2022-07-01 17:08:42 +0300

    8

--- freecodecamp/Record Collection.js
+++ freecodecamp/Record Collection.js
@@ -35,7 +35,7 @@ function updateRecords(records, id, prop, value) {
   if (prop === "artist" && value === "") { delete records[id][prop]; return records; }
   if (prop === "tracks" && value === "") { delete records[id][prop]; return records; }
 }
-console.log(recordCollection['5439']);
+
 // updateRecords(recordCollection, '5439', 'artist', 'ABBA');
 // updateRecords(recordCollection, '5439', "tracks", "Take a Chance on Me");
 // updateRecords(recordCollection, '2548', 'artist', '');
@@ -43,7 +43,5 @@ console.log(recordCollection['5439']);
 // updateRecords(recordCollection, '1245', "albumTitle", "Riptide");
 // updateRecords(recordCollection, '1245', 'tracks', ["Addicted to Love"]);
 // updateRecords(recordCollection, '2468', 'tracks', "Free");
-// recordCollection[2468].tracks[2]="neg";//[record][id].tracks[2]="neg";
-// console.log(recordCollection[2468].tracks[3])
-// console.log(recordCollection['5439']);
-// After updateRecords(recordCollection, 2468, "tracks", "Free"), tracks should have the string 1999 as the first element.
+
+console.log(recordCollection);
