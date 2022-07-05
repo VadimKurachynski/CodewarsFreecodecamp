fa7edaddda2fd2b389c55d1004f2eaeacdf8d36e
VadimKurachynski <kurachynski@gmail.com>
2022-07-05 19:24:54 +0300

    3d

--- codewars/Find the first non-consecutive number.js
+++ codewars/Find the first non-consecutive number.js
@@ -3,17 +3,11 @@ function firstNonConsecutive(arr) {
   for (i = 0; i < arr.length - 1; i++) {
     massN.push(arr[i + 1] - arr[i]);
   }
-  console.log(massN);
   const index = massN.findIndex(el => massN.indexOf(el) === massN.lastIndexOf(el));
-  return index;
   if (index === -1) return 0;
   return arr[index + 1];
-
-
-
-
 }
-//console.log(firstNonConsecutive([-5,-4,-2,0,1,2,3]));
+console.log(firstNonConsecutive([1, 2, 3, 4, 5, 8, 9]));
 // let b = [1, 2, 2, 1, 1, 1];
 // let n = 1;
 // let x1 = b.indexOf(n);
