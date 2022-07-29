a2f7603b2448a65e6ec8264932df03c39ed9b486
VadimKurachynski <kurachynski@gmail.com>
2022-07-29 21:19:27 +0300

    9u

---
+++ codewars/Removing Elements.js
@@ -0,0 +1,10 @@
+
+function removeEveryOther(arr) {
+    for (let i=0; i<arr.length;i++){
+        arr.splice(i+1,1)
+    }
+return arr
+}
+
+
+console.log(removeEveryOther([['Goodbye'], {'Great': 'Job'}]))
