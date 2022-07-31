d1fbdaf0c4287c1d94b0b21a3bba4e46cdac5deb
VadimKurachynski <kurachynski@gmail.com>
2022-07-31 19:59:07 +0300

    6nr

---
+++ codewars/tosort.js
@@ -0,0 +1,10 @@
+
+function twoSort(s) {
+return s.sort()[0].split("").join("***")
+
+  //  return 1
+}
+console.log( twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]) )
+
+
+//twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]), 'b***i***t***c***o***i***n' );
