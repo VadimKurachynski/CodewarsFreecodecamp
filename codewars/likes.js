cd04022f11603dd1919b55f8e708054349c82e60
VadimKurachynski <kurachynski@gmail.com>
2022-08-18 15:38:54 +0300

    6s

---
+++ codewars/likes.js
@@ -0,0 +1,22 @@
+
+function likes(names) {
+
+  if (names.length == 0) { return `no one likes this` }
+  if (names.length == 1) { return `${names[0]} likes this` }
+  if (names.length == 2) { return `${names[0]} and ${names[1]} like this` }
+  if (names.length == 3) { return `${names[0]}, ${names[1]} and ${names[2]} like this` }
+  if (names.length >= 4) { return `${names[0]}, ${names[1]} and ${names.length - 2} others like this` }
+
+}
+console.log(likes(['Max', 'John', 'Mark']))
+
+
+
+
+
+
+  // assert.strictEqual(likes([]), 'no one likes this');
+  // assert.strictEqual(likes(['Peter']), 'Peter likes this');
+  // assert.strictEqual(likes(['Jacob', 'Alex']), 'Jacob and Alex like this');
+  // assert.strictEqual(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this');
+  // assert.strictEqual(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this');
